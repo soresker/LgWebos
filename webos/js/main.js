c16657ea42c63d42dad5d1b60215fb91d94c7402
Ömer Selvi <omer.selvi@peple.com.tr>
2024-01-20 22:30:14 +0300

    synchronous  download method

--- webos/js/main.js
+++ webos/js/main.js
@@ -7,32 +7,34 @@ var publishmentsDir = defaultDir + 'publishments/';
 var contentsDir = defaultDir + 'contents/';
 var defaultsPort = "http://127.0.0.1:9080/file://internal/contents/"
 var connection = null;
+var urlArray;
+var currentIndex = 0
 
 function listener(event) {
 	_log("Html message coming", event.data);
-	messageCheck( event.data);
+	messageCheck(event.data);
 }
 
-const CreateIframeElement = (source,divId) => { 
-  
-	var el = document.createElement("iframe"); 
+const CreateIframeElement = (source, divId) => {
+
+	var el = document.createElement("iframe");
 
 	// setting the values for the attributes. 
-	el.src = source; 
-	el.width = "100%"; 
-	el.height = "100%"; 
+	el.src = source;
+	el.width = "100%";
+	el.height = "100%";
 
 	// Adding the created iframe to div as a child element 
-	document.getElementById(divId).appendChild(el); 
+	document.getElementById(divId).appendChild(el);
 
 }
 
-const RemoveIframeElement = (divId) => { 
+const RemoveIframeElement = (divId) => {
 	// Remove the last child ( iframe element ) of div. 
-	document.getElementById(divId) 
-		.removeChild(document 
-		.getElementById(divId).lastChild); 
-} 
+	document.getElementById(divId)
+		.removeChild(document
+			.getElementById(divId).lastChild);
+}
 
 
 window.onload = function () {
@@ -67,89 +69,88 @@ window.onload = function () {
 
 		if (result) {
 
-		//Player is registered
-		_log('player register');
+			//Player is registered
+			_log('player register');
 
-		var publishment = WebosSettings.value("Publishment/NewVersion","");
+			var publishment = WebosSettings.value("Publishment/NewVersion", "");
 
-		_log('publishment:',publishment);
+			_log('publishment:', publishment);
 
-		readfile(publishment);
+			readfile(publishment);
 
 		} else {
 			_log('player register degil');
-			 CreateIframeElement("Login/login.html","login");
-			 setTimeout(() => {
-				_log('webos ip:',webOsIp);
-				_log('webos internet active:',isInternetActive);
-			 }, 3000);
-		
+			CreateIframeElement("Login/login.html", "login");
+			setTimeout(() => {
+				_log('webos ip:', webOsIp);
+				_log('webos internet active:', isInternetActive);
+			}, 3000);
+
 		}
 
 	});
 
 	sendHardbitSystemInfo();
-/*
-	setTimeout(() => {
-		CreateIframeElement("Playing/player.html","play");
+	/*
 		setTimeout(() => {
-			RemoveIframeElement("login");
-		}, 3000);
-	},5000);
-*/
+			CreateIframeElement("Playing/player.html","play");
+			setTimeout(() => {
+				RemoveIframeElement("login");
+			}, 3000);
+		},5000);
+	*/
 }
 
 function messageCheck(msg) {
 
-	 msg = JSON.parse(msg);
-	 _log("Message Check:",msg);
-	 _log("Message MessageType:",msg.MessageType);
+	msg = JSON.parse(msg);
+	_log("Message Check:", msg);
+	_log("Message MessageType:", msg.MessageType);
 
-    switch (msg.MessageType) {
+	switch (msg.MessageType) {
 
-        case commandMessage.Player_Register:
-			_log("Player_Register yapacaz",msg);
+		case commandMessage.Player_Register:
+			_log("Player_Register yapacaz", msg);
 			playerRegister(msg);
-            break;
+			break;
 
-        case commandMessage.PlayingLog:
-            break;    
+		case commandMessage.PlayingLog:
+			break;
 
-        case commandMessage.PlayerReady:
-            break;
-           
-        default:
-            break;
-    }
+		case commandMessage.PlayerReady:
+			break;
+
+		default:
+			break;
+	}
 }
 
 function readfile(fileName) {
 	_log('readfile:', fileName);
-	var path = publishmentsDir+fileName+".json";
+	var path = publishmentsDir + fileName + ".json";
 	_log('read file path:', path);
-	fs.readFile(path,function (readData) {
+	fs.readFile(path, function (readData) {
 		_log('readfile:', readData);
 		var Data = readData;
-		if (readData )
-		{
+		if (readData) {
 			document.getElementById('iframe').contentWindow.postMessage(JSON.stringify({ "MessageType": "initPlayer", "Data": { "filePath": "http://127.0.0.1:9080/file://internal/contents/", "videoMode": "0" } }), '*');
 
 			document.getElementById('iframe').contentWindow.postMessage(JSON.stringify({
-				"MessageType": "startPublishment",Data
+				"MessageType": "startPublishment", Data
 			}), "*")
-			
-		}		
+
+		}
 		else
 			_log('not read json data:', data);
 	})
 }
 
-function writefile(fileName,data) {
+function writefile(fileName, data) {
 	_log('writefile path:', fileName);
 
-	var path = publishmentsDir+fileName+".json";
+	var path = publishmentsDir + fileName + ".json";
 	_log('writefile path:', path);
-	fs.writeFile(path,data, function (error) {
+	fs.writeFile(path, data, function (error) {
 		if (error)
 			return _log('error', error);
 		else
@@ -157,14 +158,31 @@ function writefile(fileName,data) {
 	})
 }
 
-function download(url) {
-	_log('download start:', url);
+function download(url, callback) {
 	downloader.start({
 		url: url,
-		path: defaultDir + contentsDir,
+		path: contentsDir,
 	}, function (error, data) {
-		_log('complete', error, data)
+		_log('download complete', error, data)
+		callback(error, data)
 	});
+}
+
+
+function downloadNext() {
+	var currentUrl = urlArray[currentIndex];
+	_log('download start:', 'download status:' + (currentIndex + 1) + '/' + urlArray.length);
+	_log('download file url:', currentUrl);
+	download(currentUrl, function (err, data) {
+		console.log("call")
+		if (currentIndex < urlArray.length) {
+			downloadNext()
+			currentIndex = currentIndex + 1
+		}
+		else {
+			currentIndex = 0
+		}
+	})
 }
 
 function listDir() {
@@ -198,155 +216,147 @@ function playerRegister(data) {
 		playerName: webOsModelName,
 		customerId: data.customerId
 	}
-	
+
 	WebosSettings.setValue("Customer/id", data.customerId);
 	WebosSettings.setValue("PlayerSettings/playerName", isRegisterData.playerName);
-	WebosSettings.setValue("PlayerSettings/playerId",data.playerId);
+	WebosSettings.setValue("PlayerSettings/playerId", data.playerId);
 
-	sendSignal(commandMessage.Player_Register,isRegisterData);
-	_log("Send Player Register:",isRegisterData);
+	sendSignal(commandMessage.Player_Register, isRegisterData);
+	_log("Send Player Register:", isRegisterData);
 
 }
 
 function executeReceiveCommands(commands) {
 	_log("executeReceiveCommands");
 
-if (commands.command === commandMessage.Player_Register )
-  {
-	_log("Receive commandMessage.Player_Register ");
-	 WebosSettings.setValue("PlayerSettings/status",commands.status);
+	if (commands.command === commandMessage.Player_Register) {
+		_log("Receive commandMessage.Player_Register ");
+		WebosSettings.setValue("PlayerSettings/status", commands.status);
 
-      if (commands.status == true)
-      {
-		var isGetPublishment = {
-			playerCode: "",
-			privateKey: webOsSerialNumber,
-			publicKey: webOsSerialNumber,
-			playerId:  WebosSettings.value("PlayerSettings/playerId",""),
-			playerName: webOsModelName,
-			customerId: WebosSettings.value("Customer/id","")
+		if (commands.status == true) {
+			var isGetPublishment = {
+				playerCode: "",
+				privateKey: webOsSerialNumber,
+				publicKey: webOsSerialNumber,
+				playerId: WebosSettings.value("PlayerSettings/playerId", ""),
+				playerName: webOsModelName,
+				customerId: WebosSettings.value("Customer/id", "")
+			}
+			sendSignal(commandMessage.Check_Publishment, isGetPublishment);
 		}
-		sendSignal(commandMessage.Check_Publishment, isGetPublishment);      
-      }  
 
-  }else if (commands.command === commandMessage.Check_Publishment )
-  {
-	_log("commandMessage.Check_Publishment",commands.command);
+	} else if (commands.command === commandMessage.Check_Publishment) {
+		_log("commandMessage.Check_Publishment", commands.command);
 
-      var temp = WebosSettings.value("Publishment/NewVersion","");
-	  var playerStatus = WebosSettings.value("PlayerSettings/status","");
+		var temp = WebosSettings.value("Publishment/NewVersion", "");
+		var playerStatus = WebosSettings.value("PlayerSettings/status", "");
 
-	  if(temp == "" && playerStatus == true)
-	  {	
-		  _log("Player ilk kez ayaga kalkiyor ve yayini indirmeli:",commands);
-		  WebosSettings.setValue("Publishment/NewVersion",commands.jsonData.publishmentName);
-		  WebosSettings.setValue("Publishment/OldVersion",commands.jsonData.publishmentName); 
-		  writefile(commands.jsonData.publishmentName,commands.jsonData.publishmentData); 
-		  fetchPublishment(commands.jsonData.publishmentData); 	
-		  _log("burayageldik mi :",commands.jsonData);
+		if (temp == "" && playerStatus == true) {
+			_log("Player ilk kez ayaga kalkiyor ve yayini indirmeli:", commands);
+			WebosSettings.setValue("Publishment/NewVersion", commands.jsonData.publishmentName);
+			WebosSettings.setValue("Publishment/OldVersion", commands.jsonData.publishmentName);
+			writefile(commands.jsonData.publishmentName, commands.jsonData.publishmentData);
+			fetchPublishment(commands.jsonData.publishmentData);
+			_log("burayageldik mi :", commands.jsonData);
 
-	  }
+		}
 
-      if (temp == commands.jsonData.publishmentName && playerStatus == true)
-      {
-        _log("Publisment dosyasi indiriliyor:",commands);
-		writefile(commands.jsonData.publishmentName,commands.jsonData.publishmentData); 
-		WebosSettings.setValue("Publishment/NewVersion",commands.jsonData.publishmentName);
-        fetchPublishment(commands.jsonData.publishmentData);
-      }else{
-        _log("Devamke :)");      
-      }
-   
-  }else if (commands.command === commandMessage.WinScreenShotRequest ){
-	_log("WinScreenShotRequest");  
-	//WebosDevice.screenShot();    
-  }
-  else if (commands.command === commandMessage.Player_Restart ){
-	_log("Player_Restart");   
-	//WebosDevice.deviceRestart();
-  }
-  else if (commands.command === commandMessage.Player_Shutdown ){
-	_log("Player_Shutdown");      
-	//WebosDevice.deviceShutDown();
-  }
-  else if (commands.command === commandMessage.Check_Upgrade ){
-	_log("Check_Upgrade");    
-	//WebosDevice.upgradeIpkApplication();  
-  }
-  else if (commands.command === commandMessage.PlayerDeleted ){
-	_log("PlayerDeleted");      
-	localStorage.clear();
-  }
-  else if (commands.command === commandMessage.HealthCheck ){
-	_log("HealthCheck");      
-  }
-  else if (commands.command === commandMessage.AppRestart ){
-	_log("AppRestart"); 
-	//WebosDevice.restartApplication();    
-  }
-  else if (commands.command === commandMessage.PlayerSettingsHere ){
-	_log("PlayerSettingsHere"); 
-	//WebosDevice.setPortraitMode();      
-  }
-  else{
-	_log("UNKNOWN Command");
-  }
+		if (temp == commands.jsonData.publishmentName && playerStatus == true) {
+			_log("Publisment dosyasi indiriliyor:", commands);
+			writefile(commands.jsonData.publishmentName, commands.jsonData.publishmentData);
+			WebosSettings.setValue("Publishment/NewVersion", commands.jsonData.publishmentName);
+			fetchPublishment(commands.jsonData.publishmentData);
+		} else {
+			_log("Devamke :)");
+		}
+
+	} else if (commands.command === commandMessage.WinScreenShotRequest) {
+		_log("WinScreenShotRequest");
+		//WebosDevice.screenShot();    
+	}
+	else if (commands.command === commandMessage.Player_Restart) {
+		_log("Player_Restart");
+		//WebosDevice.deviceRestart();
+	}
+	else if (commands.command === commandMessage.Player_Shutdown) {
+		_log("Player_Shutdown");
+		//WebosDevice.deviceShutDown();
+	}
+	else if (commands.command === commandMessage.Check_Upgrade) {
+		_log("Check_Upgrade");
+		//WebosDevice.upgradeIpkApplication();  
+	}
+	else if (commands.command === commandMessage.PlayerDeleted) {
+		_log("PlayerDeleted");
+		localStorage.clear();
+	}
+	else if (commands.command === commandMessage.HealthCheck) {
+		_log("HealthCheck");
+	}
+	else if (commands.command === commandMessage.AppRestart) {
+		_log("AppRestart");
+		//WebosDevice.restartApplication();    
+	}
+	else if (commands.command === commandMessage.PlayerSettingsHere) {
+		_log("PlayerSettingsHere");
+		//WebosDevice.setPortraitMode();      
+	}
+	else {
+		_log("UNKNOWN Command");
+	}
 
 }
 
 function fetchPublishment(readPublishment) {
 
-	_log("fetchPublishment :)");      
+	_log("fetchPublishment :)");
 
 	var Data = readPublishment;
- 	var urlArray = readPublishment.urlArray;
+	urlArray = readPublishment.urlArray;
 
+	downloadNext();
 
-	 urlArray.forEach((item, key) => {
-	 	download(item);
-	 });
+	_log("fetchPublishment download sonrasi:)");
 
-	 _log("fetchPublishment download sonrasi:)");      
+	document.getElementById('iframe').contentWindow.postMessage(JSON.stringify({ "MessageType": "initPlayer", "Data": { "filePath": "http://127.0.0.1:9080/file://internal/contents/", "videoMode": "0" } }), '*');
 
-	 document.getElementById('iframe').contentWindow.postMessage(JSON.stringify({ "MessageType": "initPlayer", "Data": { "filePath": "http://127.0.0.1:9080/file://internal/contents/", "videoMode": "0" } }), '*');
+	document.getElementById('iframe').contentWindow.postMessage(JSON.stringify({
+		"MessageType": "startPublishment", Data
+	}), "*")
 
-	 document.getElementById('iframe').contentWindow.postMessage(JSON.stringify({
-		"MessageType": "startPublishment",Data
-	 }), "*")
-	 
 }
 
 sendHardbitSystemInfo = (() => {
 
-	  setInterval(() => {
+	setInterval(() => {
 		_log("sendHardbitSystemInfo");
 		var systemInfData = {
 			playerCode: "",
 			privateKey: webOsSerialNumber,
 			publicKey: webOsSerialNumber,
-			playerId: WebosSettings.value("PlayerSettings/playerId",""),
+			playerId: WebosSettings.value("PlayerSettings/playerId", ""),
 			playerName: webOsModelName,
-			customerId: WebosSettings.value("Customer/id","")
+			customerId: WebosSettings.value("Customer/id", "")
 
 		}
 
-		sendSignal(commandMessage.HealthCheck, systemInfData);      
-	  }, 10000);
-  
+		sendSignal(commandMessage.HealthCheck, systemInfData);
+	}, 10000);
+
 });
 
 sendScreenShot = ((base64Image) => {
 
-	  _log("sendScreenShot");
-	  var playerInfo = {
-		  privateKey: webOsSerialNumber,
-		  publicKey: webOsSerialNumber,
-		  playerId: WebosSettings.value("PlayerSettings/playerId",""),
-		  customerId: WebosSettings.value("Customer/id",""),
-		  base64Image: base64Image,
-		  screenResolution: '400x300',
-	  }
+	_log("sendScreenShot");
+	var playerInfo = {
+		privateKey: webOsSerialNumber,
+		publicKey: webOsSerialNumber,
+		playerId: WebosSettings.value("PlayerSettings/playerId", ""),
+		customerId: WebosSettings.value("Customer/id", ""),
+		base64Image: base64Image,
+		screenResolution: '400x300',
+	}
 
-	  sendSignal(commandMessage.Win_ScreenShot, playerInfo);      
+	sendSignal(commandMessage.Win_ScreenShot, playerInfo);
 
 });
